61761084c092235a62c9225183c2d0738197cfc4
GraczykMateusz <43554417+GraczykMateusz@users.noreply.github.com>
2021-07-14 20:37:37 +0200

    Initial commit

---
+++ src/test/java/pl/graczykmateusz/BowlingGameKata/BowlingGameKataApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.graczykmateusz.BowlingGameKata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BowlingGameKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
