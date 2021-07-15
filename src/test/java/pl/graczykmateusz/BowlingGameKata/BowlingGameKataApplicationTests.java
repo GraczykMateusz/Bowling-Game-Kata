e2006a81a0e75d0f9971977f7b704f54e96ad063
GraczykMateusz <43554417+GraczykMateusz@users.noreply.github.com>
2021-07-15 10:33:32 +0200

    {RED} should score equals zero when miss all rounds

--- src/test/java/pl/graczykmateusz/BowlingGameKata/BowlingGameKataApplicationTests.java
+++ src/test/java/pl/graczykmateusz/BowlingGameKata/BowlingGameKataApplicationTests.java
@@ -1,13 +1,28 @@
 package pl.graczykmateusz.BowlingGameKata;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BowlingGameKataApplicationTests {
+	Game game;
 
-	@Test
-	void contextLoads() {
+	@BeforeEach
+	void setup() {
+		game = new Game();
 	}
 
+	@Test
+	void shouldScoreEqualsZeroWhenMissAllRounds() {
+		final int expected = 0;
+		final int rounds = 20;
+
+		for (int i = 0; i < rounds; i++) {
+			game.roll(0);
+		}
+		int actual = game.score();
+		Assertions.assertEquals(expected, actual);
+	}
 }
