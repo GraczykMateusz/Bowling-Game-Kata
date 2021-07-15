89bb11a2ee72f46872b2709e353e9f8e0f8c934b
GraczykMateusz <43554417+GraczykMateusz@users.noreply.github.com>
2021-07-15 12:25:04 +0200

    {GREEN} should score equals sum of pins when no rolled any bonuses like strike or spare per round

--- src/main/java/pl/graczykmateusz/BowlingGameKata/Game.java
+++ src/main/java/pl/graczykmateusz/BowlingGameKata/Game.java
@@ -1,14 +1,54 @@
 package pl.graczykmateusz.BowlingGameKata;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Game implements IGame {
+    int round = 1;
+    final int rollPerRound = 2;
+
+    List<List<Integer>> scorePerFrames = new ArrayList<>();
+    List<Integer> scorePerFrame = new ArrayList<>();
+
+    final int strike = 10;
+    final int spare = 10;
+
+    boolean isStrike = false;
+    boolean isSpare = false;
 
     @Override
     public void roll(int pins) {
+        scorePerFrame.add(pins);
 
+        if (round % rollPerRound == 0) {
+            scorePerFrames.add(scorePerFrame.stream().collect(Collectors.toList()));
+            scorePerFrame.clear();
+        }
+        nextRound();
     }
 
     @Override
     public int score() {
-        return 0;
+        return scorePerFrames.stream()
+                .flatMap(List::stream)
+                .reduce(Integer::sum)
+                .get();
+    }
+
+    private void nextRound() {
+        round++;
+    }
+
+    private boolean checkStrike(int pins) {
+        if (pins == strike) {
+            return true;
+        }
+        return false;
+    }
+
+    private void strike(int pins) {
+//        temp.add(pins);
+//        nextRound();
     }
 }
