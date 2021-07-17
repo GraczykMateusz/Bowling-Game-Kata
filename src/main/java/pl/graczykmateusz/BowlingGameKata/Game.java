c1ba37ecff9a6cc4b15871e306ece59cdec44ce4
GraczykMateusz <43554417+GraczykMateusz@users.noreply.github.com>
2021-07-17 15:17:04 +0200

    {GREEN} Another try to solve bowling game

--- src/main/java/pl/graczykmateusz/BowlingGameKata/Game.java
+++ src/main/java/pl/graczykmateusz/BowlingGameKata/Game.java
@@ -4,35 +4,50 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Game implements IGame {
-    boolean isGameOver = false;
-    
+    List<Integer> rolls = new ArrayList<>();
+    int score = 0;
+    int rollIndex = 0;
 
     @Override
-    public void roll(int pins) {
-        if (isGameOver) {
-
-        } else {
-            throw new IllegalStateException("Cannot roll when the game is over!");
-        }
+    public void roll(int pin) {
+        checkPinValidation(pin);
+        rolls.add(pin);
     }
 
     @Override
     public int score() {
-
+        return calculateScore();
     }
 
-    private boolean checkIfIsStrike(int pins) {
-
+    int calculateScore() {
+        final int framesCount = 10;
+        for (int i = 0; i < framesCount; i++) {
+            if (checkIfIsStrike()) {
+                score += 10 + rolls.get(rollIndex + 1) + rolls.get(rollIndex + 2);
+                rollIndex++;
+            } else if (checkIfIsSpare()) {
+                score += 10 + rolls.get(rollIndex + 2);
+                rollIndex += 2;
+            } else {
+                score += rolls.get(rollIndex) + rolls.get(rollIndex + 1);
+                rollIndex += 2;
+            }
+        }
+        return score;
     }
 
-    private boolean checkIfIsSpare() {
-
+    boolean checkIfIsStrike() {
+        return rolls.get(rollIndex) == 10;
     }
 
-    private void checkIfPinsAreValid(int pins) {
-        if (pins < 0)
+    boolean checkIfIsSpare() {
+        return rolls.get(rollIndex) + rolls.get(rollIndex + 1) == 10;
+    }
+
+    void checkPinValidation(int pin) {
+        if (pin < 0)
             throw new IllegalArgumentException("Pins cannot be smaller then 0");
-        if (pins > 10)
+        if (pin > 10)
             throw  new IllegalArgumentException("Pins cannot be greater then 10");
     }
 }
